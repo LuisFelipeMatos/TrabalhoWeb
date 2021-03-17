5a2bc3f6f28cea780ae11ba88af64eaa8d670b95
LuisFelipeMatos <60949831+LuisFelipeMatos@users.noreply.github.com>
2021-03-17 15:56:31 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const tags = require('./routes/tag')
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const fs = require('fs');
+const https = require('https');
+const portaHttps = 443;
 
 
 
@@ -15,7 +18,16 @@ app.use('/usuario', usuario);
 app.use('/nota', nota);
 app.use(bodyParser.json());
 
+const key = fs.readFileSync('certs/localhost-key.pem');
+const cert = fs.readFileSync('certs/localhost.pem');
 
+const credentials = { key, cert};
+const httpsServer = https.createServer(credentials, app);
+
+
+httpsServer.listen(potaHttps, () => {
+    console.log(`API rondando seguramente na porta ${portaHttps}`);
+});
 
 
 
